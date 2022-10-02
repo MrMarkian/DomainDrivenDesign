d4f658491bc93894b5c44fa5a8b434238b989b7d
Markian Rutkowskyj <i-keep-laughing@myself.com>
2022-10-02 14:53:37 +0100

    Value objects

---
+++ MarketPlace.Domain/UserId.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MarketPlace.Domain
+{
+    public class UserId
+    {
+
+        private readonly Guid _value;
+
+        public UserId(Guid value)
+        {
+            if (value == default)
+            {
+                throw new ArgumentException(nameof(value), "User Id Cannot be emtpy.");
+                
+            }
+
+            _value = value;
+        }
+
+    }
+}
