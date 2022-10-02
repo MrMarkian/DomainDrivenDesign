957f484dfb149ef24e4bf5c2091b20380e44e5a1
Markian Rutkowskyj <i-keep-laughing@myself.com>
2022-10-02 15:23:53 +0100

    Added Unit Test (fail example)

---
+++ MarketPlace.Tests/MoneyTest.cs
@@ -0,0 +1,23 @@
+﻿using NuGet.Frameworks;
+
+namespace MarketPlace.Tests;
+using Xunit;
+using MarketPlace.Domain;
+
+
+public class MoneyTest
+{
+    [Fact]
+    public void
+        Money_objects_with_the_same_amount_should_be_equal()
+    {
+        var firstAmount = new Money(5);
+        var secondAmount = new Money(5);
+        
+        Assert.Equal(firstAmount, secondAmount);
+        
+        
+        
+
+    }
+}
